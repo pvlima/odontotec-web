5f39588343b105fa39acae95152688ba2fe62275
Paulo Vitor <pv.lima02@gmail.com>
2020-08-21 12:37:35 -0300

    Creating Button Component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.button`
+  width: 100%;
+  background-color: var(--color-bg-primary);
+  border-radius: 8px;
+  margin-top: 16px;
+  padding: 16px;
+  border: none;
+  color: #fff;
+  font-weight: 500;
+  transition: background-color 0.2s;
+
+  &:hover {
+    background-color: var(--color-bg-primary-shade);
+  }
+`;
