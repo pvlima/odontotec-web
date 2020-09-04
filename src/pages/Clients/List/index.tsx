0b5613dcddab50474f4e15f690a03de03f4bb7ba
Paulo Vitor <pv.lima02@gmail.com>
2020-09-04 12:58:48 -0300

    chore: List Clients search api

--- src/pages/Clients/List/index.tsx
+++ src/pages/Clients/List/index.tsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { differenceInYears } from 'date-fns';
 
 import Header from '../../layout/Header';
 
 import { Container, ContainerHeader } from './styles';
+import api from '../../../services/api';
+
+interface IClientData {
+  id: string;
+  name: string;
+  phone: string;
+  birth: Date;
+  city?: string;
+  age?: number;
+}
 
 const List: React.FC = () => {
+  const [clients, setClients] = useState<IClientData[]>([]);
+
+  useEffect(() => {
+    const loadClients = async () => {
+      const response = await api.get<IClientData[]>('/clients', {
+        headers: {
+          authorization:
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1OTkyMjkxNDIsImV4cCI6MTU5OTMxNTU0Miwic3ViIjoiYWYzZDVhM2QtYTM2My00NTVhLTg5ZTMtN2UzMTg2ZjI4YjA5In0.3YzO5L23yc4oZXnmv0x162CjOtqwbA4nPnA10ilUtV8',
+        },
+      });
+
+      const data = response.data.map(item => ({
+        ...item,
+        age: item.birth && differenceInYears(new Date(), new Date(item.birth)),
+      }));
+
+      setClients(data);
+    };
+
+    loadClients();
+  }, []);
+
   return (
     <>
       <Header />
@@ -19,31 +52,20 @@ const List: React.FC = () => {
           <thead>
             <tr>
               <th>Nome</th>
+              <th>Telefone</th>
               <th>Idade</th>
               <th>Cidade</th>
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Paulo Vitor Correia Lima</td>
-              <td>27</td>
-              <td>Carnaubal</td>
-            </tr>
-            <tr>
-              <td>Paulo Vitor Correia Lima</td>
-              <td>27</td>
-              <td>Carnaubal</td>
-            </tr>
-            <tr>
-              <td>Paulo Vitor Correia Lima</td>
-              <td>27</td>
-              <td>Carnaubal</td>
-            </tr>
-            <tr>
-              <td>Paulo Vitor Correia Lima</td>
-              <td>27</td>
-              <td>Carnaubal</td>
-            </tr>
+            {clients.map(client => (
+              <tr key={client.id}>
+                <td>{client.name}</td>
+                <td>{client.phone}</td>
+                <td>{client.age ? client.age : '--'}</td>
+                <td>{client.city ? client.city : '--'}</td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </Container>
